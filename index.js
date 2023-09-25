5da5b6933f2c8b27660f1c973edc8fc48d3331d0
Martin <mmaina290@gmail.com>
2023-09-25 21:23:17 +0300

    Concatenation

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+//currentUser = 'Martin';
+const currentUser = 'Martin';
+
+const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+console.log(welcomeMessage);
+
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + '!';
+
+const shortGreeting = "Welcome, " + currentUser[0]  + "!";
+
